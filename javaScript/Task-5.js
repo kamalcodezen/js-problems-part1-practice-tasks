089aca252837c13de2566672c658956ec56314c3
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-09 19:37:24 +0530

    task 5 complete

---
+++ javaScript/Task-5.js
@@ -0,0 +1,7 @@
+// Generate a random number between 10 to 20.
+
+console.log((Math.random() * (20 - 10) + 10));
+
+
+// integer 10 - 20;
+console.log(Math.floor(Math.random() * (20 - 10 + 1) + 10));
